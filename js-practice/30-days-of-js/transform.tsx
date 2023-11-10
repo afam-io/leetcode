6a83e9268b1562e246b9b5df8348cc72a565ebf5
Afam Okoh <afamsdev@gmail.com>
2023-11-10 18:53:09 +0300

    feat: transform

---
+++ js-practice/30-days-of-js/transform.tsx
@@ -0,0 +1,16 @@
+/* 
+Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
+
+The returned array should be created such that returnedArray[i] = fn(arr[i], i).
+
+Please solve it without the built-in Array.map method.
+*/
+
+const map = (
+  arr: number[],
+  fn: (n: number, i?: number) => number
+): number[] => {
+  const ans: number[] = [];
+  arr.forEach((x, i) => ans.push(fn(x, i)));
+  return ans;
+};
